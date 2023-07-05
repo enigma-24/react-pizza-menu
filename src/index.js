51361cf2008baee2f09cc4ff52d4713eec2ed17d
Aniket <aniket.d2402@gmail.com>
2023-07-04 21:52:07 -0400

    final code changes done

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './index.css';
 
 const pizzaData = [
   {
@@ -46,19 +47,92 @@ const pizzaData = [
   },
 ];
 
+const Header = () => {
+  return (
+    <header className='header'>
+      <h1>Fast React Pizza Company</h1>
+    </header>
+  );
+};
 
-const Pizza = () => (
-  <div>
-    <img src="pizzas/spinaci.jpg" alt="pizza spinaci" />
-    <h2>Pizza Spinaci</h2>
-    <p>Tomato, mozarella, spinach, and ricotta cheese</p>
+const Menu = () => {
+  const pizzas = pizzaData;
+
+  return (
+    <main className='menu'>
+      <h2>Our Menu</h2>
+
+      {pizzas.length > 0 ? (
+        <>
+          <p>
+            Authenticate Italian cuisine. 6 creative dishes to choose from. All
+            from our stone oven, all organic, all delicious.
+          </p>
+          <ul className='pizzas'>
+            {pizzas.map((pizza) => (
+              <Pizza key={pizza.name} pizzaObj={pizza} />
+            ))}
+          </ul>
+        </>
+      ) : (
+        <p>We are still working on our menu. Please come back later!</p>
+      )}
+    </main>
+  );
+};
+
+const Pizza = (props) => {
+  const { photoName, name, ingredients, price, soldOut } = props.pizzaObj;
+
+  return (
+    <li className={`pizza ${soldOut && 'sold-out'}`}>
+      <img src={photoName} alt='pizza spinaci' />
+      <div>
+        <h3>{name}</h3>
+        <p>{ingredients}</p>
+        <span>{soldOut ? 'SOLD OUT!' : price}</span>
+      </div>
+    </li>
+  );
+};
+
+const Order = ({ closeHour, openHour }) => (
+  <div className='order'>
+    <p>
+      We are open from {openHour}:00 until {closeHour}:00. Come visit us or
+      order online
+    </p>
+    <button className='btn'>Order</button>
   </div>
 );
 
+const Footer = () => {
+  const hour = new Date().getHours();
+  const openHour = 12;
+  const closeHour = 22;
+  const isOpen = hour >= openHour && hour <= closeHour;
+
+  // if (hour >= openHour && hour <= closeHour) alert('We are open!');
+  // else alert('Sorry! We are closed!');
+
+  return (
+    <footer className='footer'>
+      {isOpen ? (
+        <Order closeHour={closeHour} openHour={openHour} />
+      ) : (
+        <p>
+          We are happy to welcome you between {openHour}:00 and {closeHour}:00
+        </p>
+      )}
+    </footer>
+  );
+};
+
 const App = () => (
-  <div>
-    <h1>Hello React!</h1>
-    <Pizza />
+  <div className='container'>
+    <Header />
+    <Menu />
+    <Footer />
   </div>
 );
 
